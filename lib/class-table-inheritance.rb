a32a71867bafba76cbb65ed4c551c9d4e4f591d9
Bruno Frank Cordeiro <bfscordeiro@gmail.com>
2010-11-30 19:22:12 -0200

    Conclusion of polymorphic

--- lib/class-table-inheritance.rb
+++ lib/class-table-inheritance.rb
@@ -5,24 +5,38 @@ require 'inherits-migration'
 # simple multiple table (class) inheritance.
 class ActiveRecord::Base  
   attr_reader :reflection
-      
-  def self.acts_as_subclass
-    if self.column_names.include?("type")
+
+  def self.acts_as_superclass
+    if self.column_names.include?("subtype")
       def self.find(*args)
-        old = super
-        puts self.inspect
-        if !old[:type].nil? && !old[:type].blank?
-          inherits_type = Object.const_get(old[:type].to_s)
-          inherits_type.send(:find, *args)
-        else
-          super
+        super_classes = super
+        begin
+          if super_classes.kind_of? Array
+            super_classes.map do |item|
+              if !item.subtype.nil? && !item.subtype.blank?
+                inherits_type = Object.const_get(item.subtype.to_s)
+                inherits_type.send(:find, *args)
+              else
+                super_classes
+              end
+            end
+          else
+            if !super_classes.subtype.nil? && !super_classes.subtype.blank?
+              inherits_type = Object.const_get(super_classes.subtype.to_s)
+              inherits_type.send(:find, *args)
+            else
+              super_classes
+            end
+          end
+        rescue
+          super_classes
         end
       end
     end  
   end
-      
-  def self.inherits_from(association_id)
 
+  def self.inherits_from(association_id)
+    
     # add an association, and set the foreign key.
     has_one association_id, :foreign_key => :id
 
@@ -37,19 +51,18 @@ class ActiveRecord::Base
       send("#{association_id}_without_autobuild") || send("build_#{association_id}")
     end
 
-    
+  
     # Set a method chain whith autobuild.
     alias_method_chain association_id, :autobuild    
 
-    
+  
     # bind the before save, this method call the save of association, and
     # get our generated ID an set to association_id field.
     before_save :save_inherit
-    
-    
+  
+  
     # Bind the validation of association.
     validate :inherit_association_must_be_valid    
-
 
     # Generate a method to validate the field of association.    
     define_method("inherit_association_must_be_valid") do
@@ -60,12 +73,12 @@ class ActiveRecord::Base
           errors.add(attr, message)
         end
       end
-      
+    
       valid
     end    
-          
-    
-    
+        
+  
+  
     # get the class of association by reflection, this is needed because
     # i need to get the methods and attributes to make a proxy methods.
     reflection = create_reflection(:has_one, association_id, {}, self)
@@ -74,21 +87,21 @@ class ActiveRecord::Base
     inherited_columns = association_class.column_names
     # Make a filter in association colluns to exclude the colluns that
     # the generalized class already have.
-    inherited_columns = inherited_columns.reject { |c| self.column_names.grep(c).length > 0 || c == "type" }
+    inherited_columns = inherited_columns.reject { |c| self.column_names.grep(c).length > 0 || c == "type" || c == "subtype"}
     # Get the methods of the association class and tun it to an Array of Strings.
     inherited_methods = association_class.reflections.map { |key,value| key.to_s }
     # Make a filter in association methods to exclude the methods that
     # the generalizae class already have.
     inherited_methods = inherited_methods.reject { |c| self.reflections.map {|key, value| key.to_s }.include?(c) }
-    
-    
+  
+  
     # create the proxy methods to get and set the properties and methods
     # in association class.
     (inherited_columns + inherited_methods).each do |name|
     	define_method name do
-	  	  # if the field is ID than i only bind that with the association field.
-	  	  # this is needed to bypass the overflow problem when the ActiveRecord
-	  	  # try to get the id to find the association.
+    	  # if the field is ID than i only bind that with the association field.
+    	  # this is needed to bypass the overflow problem when the ActiveRecord
+    	  # try to get the id to find the association.
     	  if name == 'id'
     	    self["#{association_id}_id"]
     	  else 
@@ -96,12 +109,12 @@ class ActiveRecord::Base
           assoc.send(name)
         end
     	end
-  
-    	
+
+  	
     	define_method "#{name}=" do |new_value|
-	  	  # if the field is ID than i only bind that with the association field.
-	  	  # this is needed to bypass the overflow problem when the ActiveRecord
-	  	  # try to get the id to find the association.
+    	  # if the field is ID than i only bind that with the association field.
+    	  # this is needed to bypass the overflow problem when the ActiveRecord
+    	  # try to get the id to find the association.
     	  if name == 'id'
     	    self["#{association_id}_id"] = new_value
     	  else     	  
@@ -117,10 +130,9 @@ class ActiveRecord::Base
     # generalized class.
     define_method("save_inherit") do |*args|
       association = send(association_id)
-      if !association.column_for_attribute("type").nil?
-        association.type = self.class.to_s
+      if association.attribute_names.include?("subtype")
+        association.subtype = self.class.to_s
       end
-      
       association.save
       self["#{association_id}_id"] = association.id
       true
