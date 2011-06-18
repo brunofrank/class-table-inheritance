ff7b068cce02bbf082d7d65607158ab728fb7170
Matt Huggins <matt.huggins@gmail.com>
2011-06-18 16:35:12 -0500

    Fix variable referenced in loop for array of results

--- lib/class-table-inheritance.rb
+++ lib/class-table-inheritance.rb
@@ -14,7 +14,7 @@ class ActiveRecord::Base
           if super_classes.kind_of? Array
             super_classes.map do |item|
               if !item.subtype.nil? && !item.subtype.blank?
-                inherits_type = super_classes.subtype.to_s.classify.constantize
+                inherits_type = item.subtype.to_s.classify.constantize
                 inherits_type.send(:find, item.id)
               else
                 super_classes
